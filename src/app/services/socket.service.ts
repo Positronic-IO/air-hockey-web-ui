a6527bcdcd1fc385f46f3d16e7b0ef7ef9e8321d
Tony Hammack <hammack.tony@gmail.com>
2019-08-18 23:33:30 -0500

    No checkpoint

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 
 import { Socket } from 'ngx-socket-io';
-import { merge } from 'rxjs';
-import { element } from 'protractor';
 
 @Injectable()
 export class SocketService {
     $events = this.socket.fromEvent<any>('event');
     $scores = this.socket.fromEvent<any>('scores');
     $state = this.socket.fromEvent<any>('state');
-    $save = this.socket.fromEvent<any>('save');
     
     constructor(private socket: Socket) {
     }
